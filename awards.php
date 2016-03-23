25438639dbeaa490a10b1d25328fd91713d545af
Q Huang <q@hopelab.com>
2016-03-22 22:51:29 -0700

    finished first draft of awards page

--- awards.php
+++ awards.php
@@ -35,7 +35,9 @@
       </div>
     </div>
     <div class="col-sm-4">
-      
+       <img src="images/sliders_small/IMG_1977-001.JPG" class="img-responsive" style="width:100%" alt="Image">
+       <img src="images/sliders_small/DSC_8056-001.JPG" class="img-responsive" style="width:100%" alt="Image">
+       <img src="images/sliders_small/IMG_3415-001.JPG" class="img-responsive" style="width:100%" alt="Image">
     </div>
   </div>
   <div id="news" class="row">
@@ -51,7 +53,7 @@ Most Entertaining,  Secondary Large Group (Joy of Paper Cutting)
 4 National Top First Awards, Primary (Purple Fish, Joy of Learning Chinese, We've got the World, Jasmine Love)
 National Elite Top First Award, Secondary,  (Joy of Paper Cutting)
 Judges Choice Award ( (Joy of Paper Cutting)
-<em>Choreographer of the Year</em> (Ms Lu)        
+<em>Choreographer of the Year</em> (for Ms Jun Lu)        
          </pre>
          
           <b>American Dance Awards 2015</b><br>
@@ -89,6 +91,158 @@ Elite Top First for Joy Of Paper Cutting
       <img src="images/content/photo16.JPG" class="img-responsive" style="width:100%" alt="Image">
        <img src="images/content/photo7.JPG" class="img-responsive" style="width:100%" alt="Image">
        <img src="images/content/photo1.gif" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+  </div>
+  
+   <div id="news" class="row">
+    <div class="col-sm-8">
+      <div class="well">
+       <p>
+         <h4> 2014 </h4>
+         <b>KAR National Final 2014 at Las Vegas</b><br>
+         <pre>
+National Elite Top First Award (Do the CanCan)
+National Elite Top First Award (Beat It)
+National Top First Award (Little Mulan)
+Most Entertaining, Secondary Large Group (Do the CanCan)
+High Point Award (Do the CanCan)
+High Point Award (Beat It)      
+
+The award winning dancers are
+Do the CanCan: Shannon Ma, Ella Yee, Victoria Hu, Julie Shi, Jamie Tan, Ella Lan, Amy Liu, Sophia Wang, Ariel Huang, Katherine Chen. Maggie Peng
+
+Beat It: Victoria Hu, Ella Yee, Ariel Huang, Katherine Chen, Maggie Peng, Chloe Low, Caroline Wang, Vivian Zhao. Amelia Yu, Annie Shang, Adrienne Ferguson, Nina Pan
+
+Little Mulan: Chloe Low, Norina Xu, Ella Li, Lily Shi, Sydney Li, Meredith Lin, Mia Liu, Amy Miao, Clarie Luo, Daphne Zhu, Jessie Zhu
+         </pre>
+         <br>
+          <b>American Dance Awards 2014</b><br>
+         <pre>
+Do the CanCan
+-- Ultimate Gold Medal
+-- High Score Awards, Age 7-9, First place
+-- Overall high score award, Primary, 12 and under, First place
+-- Special Judge Award : "Most Fun"
+-- Overall Highest Adjudication, 12 & Under, first place 
+-- "Diamond Choreography" 
+
+Beat It
+-- Ultimate Gold Medal
+-- High score award, Age 7-9, Second place (note: "Do the CanCan" first place in this category)
+-- Special Judge Award: "Most Intensity" 
+
+Little Mulan
+-- Ultimate Gold Medal
+-- High Score awards, Age 6 & Under, Second place
+-- Special Judge Award "Most Entertaining"
+
+Galloping Spirit
+-- Ultimate Gold Medal
+-- Special Judge Award: "Most Energy"
+-- Overall high score award, primary 12 and under, Third place. (again "Do the CanCan" First place in this category)
+
+Ms Lu received the ADA's best choreography award again: "the ADA America's Choreographer of the Year, Diamond Choreography Winner 2014".
+
+And the team members are:
+(1) Do the CanCan (Shannon, Ella Y, Victoria, Julie, Amy, Ella L., Jamie, Sophia, Maggie, Katherine, Ariel)
+(2) Beat It (Ella Y, Victoria, Chloe, Ariel, Nina, Annabel, Adrienne, Amelia, Annie, Maggie, Angela, Katherine, Caroline, Macy, Vivian, Kelly)
+(3) Little Mulan (Mia Liu, Chloe Low, Lily Shi, Angela Pei, Daphne Zhu, Meredith Lin, Norina Xu, Claire Luo, Ella Li, Amy Miao, Sydney Li, Jessie Zhu) 
+(4) Galloping Spirit (Cecillia Sun, Aileen Wu, Elizabeth Ding, Nerine Uyanik, Emily Xia, Krystal Liu, Lily Jiang, Megan Xu, Caroline Wang, Kaylee Mai) 
+
+         </pre>
+         
+<b>KAR Regional 2014 at San Jose</b><br>
+         <pre>
+Do the CanCan
+- Top First Award
+- High Point Award (Primary 8 and Under Large Overall)
+
+Little Mulan
+- Top First Award
+- High Point Award (Primary 8 and Under Large Overall)
+- <em>Most Entertaining Award</em>
+- <em>Video of the Week nominee award</em>
+
+Beat It
+- Top First Award
+- Judge's Choice Award: "Fierce Focus"
+
+
+Galloping Spirit
+- Top First Award
+- High Point Award (Primary 9 to 11)
+- Won New York Experience Invitation
+
+Hey Mickey
+- Top First Award
+         </pre>
+       </p>
+      </div>
+      
+    </div>
+    
+    <div class="col-sm-4">
+      <img src="images/small/CFC_9656c.JPG" class="img-responsive" style="width:100%" alt="Image">
+       <img src="images/small/BeatIt.JPG" class="img-responsive" style="width:100%" alt="Image">
+       <img src="images/small/CFC_9858c.JPG" class="img-responsive" style="width:100%" alt="Image">
+       <img src="images/small/CFC_0052c.JPG" class="img-responsive" style="width:100%" alt="Image">
+       <img src="images/small/CFC_9555.JPG" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+  </div>
+   <div id="news" class="row">
+    <div class="col-sm-8">
+      <div class="well">
+       <p>
+         <h4> 2013 </h4>
+         
+<b>KAR Regional 2013 at San Jose</b><br>
+         <pre>         
+"Dance to the Music" won the "HIGH POINT AWARD (#1, highest)" across all styles and categories for SMALL groups ages 8 and under.  
+"Little Red Fans" won their own "HIGH POINT AWARD (#1, highest)" across all styles and categories for LARGE groups ages 8 and under.  
+"Happy Handkerchiefs" won "HIGH POINT AWARD(#5)" across all styles and categories for LARGE groups ages 8 and under.  
+"Dance of the Rainbow" won "HIGH POINT AWARD(#4)" in the solo, duo/trio category.  
+"Dance of the Rainbow" is also one of the four teams won "All Star Award" (only for dancers ages 9 and above) in the solo, duo/trio category.  
+
+"Dance to the Music" won the "MOST ENTERTAINING AWARD" across all styles and categories for SMALL groups (cross all ages and competitive levels).  
+"Little Red Fans" won the "MOST ENTERTAINING AWARD" across all styles and categories for LARGE groups (cross all ages and competitive levels).  
+that means, our teams sweep all "MOST ENTERTAINING AWARD"!  That is, there are only two "MOST ENTERTAINING AWARD" awards, and our dancers got them all!
+
+"Little Red Fans" won the regional  "Video of the Week" award. 
+"Happy Handkerchiefs" also won a judges' choice award as "the future's dancers"
+
+All of them won "Top first award" (highest) in their respective category
+
+And the dancers are:
+(1) Dance of the Rainbow (Baily Chu and Shirley Qi,  Baily was not feeling well this morning, but she still got herself together and did it!)
+(2) Dance to the Music (Amy, Sophia, Eileen, Shannon, Victoria, Julie, Ella Y, Ella L, Jamie)
+(3) Happy Handkerchiefs (Ceci, Aileen, Caroline,Amy, Olivia)
+(4) Little Red Fans(Ella, Ariel, Amelia, Vivian, Adrienne, Victoria,  Lindawest, Annabel, Sophia, Annie, Maggie, Angela, Katherine, Caroline, Ella)
+         </pre>
+         
+          <b>American Dance Awards 2013</b><br>
+         <pre>
+
+Three dance groups went to ADA 213 and won two "ULTIMATE GOLD", and one "GOLD". 
+All won the "HIGHEST OVERALL SCORE" awards in their respective competition category, 
+and one team won the "FIRST PLACE" award for all age 6 and under
+Two of the teams won "SPECIAL RECOGNITION" awards in addition for their outstanding performances. 
+Ms Lu received the best choreography award: "the ADA Diamond Choreography Winner of 2013".
+
+And the groups are:
+(1) Dance to the music (Amy, Sophia, Eileen, Shannon, Victoria, Julie, Ella Y, Ella L, Jamie)
+(2) Little Red Fan (Ella, Ariel, Amelia, Vivian, Adrienne, Victoria,  Lindawest, Annabel, Sophia, Annie, Maggie, Angela, Katherine, Ella)
+(3) Happy Handkerchiefs (Ceci, Aileen, Caroline, Amy, Olivia, Angela)
+
+Congratulations to all the teams!  
+         </pre>
+         
+       </p>
+      </div>
+    </div>
+   <div class="col-sm-4">
+      <img src="images/small/CLA_6965s.JPG" class="img-responsive" style="width:100%" alt="Image">
+       <img src="images/small/CLA_7558.JPG" class="img-responsive" style="width:100%" alt="Image">
+       <img src="images/small/CLA_6553.JPG" class="img-responsive" style="width:100%" alt="Image">
     </div>
   </div>
     <br>

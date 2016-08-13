70af14816cb1cd2904d9baaa4a03fca42db94e3b
Q Huang <q@hopelab.com>
2016-08-12 20:08:38 -0700

    added new awards info

--- awards.php
+++ awards.php
@@ -8,33 +8,60 @@
       <div class="well">
        <p>
          <h4> 2016 </h4>
-<b>NexStar national talent (dance) competition</b>
-<pre>  
-"Hey Mickey": 
-- Platinum Award & First Place
-- <em>Regional Champion</em> in Novice Level, across all age groups and categories. 
-- WILD ABOUT YOU award, 11&under
-- Invitation only "golden ticket" to world dance championship 
 
-"Paso Doble": 
-- Platinum Award & category First Place
-- 2nd place, age 9-11, large group, all categories, novice level
-- Best Costume Award
+         <b>KAR National Final 2016 at Las Vegas</b><br>
+<blockquote>
 
-"Dance to the Music":
-- Platinum Award & First Place
-- First place of 8 & under, Intermediate level, Line 
-- Invitation only "golden ticket" to world dance championship
+- <em>Grand National Champion</em>, Primary Large Groups and Line (Minion's Dance to the Music)<br>
+- 2 National Elite Top First Awards, Primary (Hey Mickey, Minion's Dance to the Music)<br>
+- 1 National Elite Top First Award, Secondary,  (Eternal Flame)<br>
+- 3 Judges Choice Award (all three routines)<br>
+</blockquote>
 
-"Eternal Flame":
-- High Gold Award & First Place in category
-- Regional 2nd Place in Intermediate Level, 9-11 age group, across categories. 
-- Invitation only "golden ticket" to world dance championship
-</pre>      
+         <b>KAR Regional 2016 </b><br>
+<blockquote>
+           - 4 Elite Top First Awards (Eternal Flame, Hey Mickey, Minion's Dance to the Music, Paso Doble)<br>
+           - Top Primary Large Group, First Place(Hey Mickey)<br>
+           - Top Primary Large Group 8 and Under, First Place(Hey Mickey)<br>
+           - Top Primary Line, First Place (Minion's Dance to the Music)<br>
+           - Top Primary Line 8 and Under, First Place (Minion's Dance to the Music)<br>
+           - The Most Entertaining Act, Primary Large Groups (Hey Mickey)<br>
+           - Top Secondary Large Group 9 - 11, Second Place (Eternal Flame)<br>
+           - Secondary Large Groups Best Costume (Eternal Flame)<br>
+           - Video of the Week Nominee and National Video of the Week Winner (Minion's Dance to the Music)<br>
+</blockquote>
+
+<b>NexStar National Talent (dance) Competition</b>
+<blockquote>
+"Hey Mickey":<br>
+- Platinum Award & First Place<br>
+- <em>Regional Champion</em> in Novice Level, across all age groups and categories.<br>
+- WILD ABOUT YOU award, 11&under<br>
+- Invitation only "golden ticket" to world dance championship<br>
+<br>
+"Paso Doble":<br>
+- Platinum Award & category First Place<br>
+- 2nd place, age 9-11, large group, all categories, novice level<br>
+- Best Costume Award<br>
+<br>
+"Dance to the Music":<br>
+- Platinum Award & First Place<br>
+- First place of 8 & under, Intermediate level, Line<br>
+- Invitation only "golden ticket" to world dance championship<br>
+<br>
+"Eternal Flame":<br>
+- High Gold Award & First Place in category<br>
+- Regional 2nd Place in Intermediate Level, 9-11 age group, across categories.<br>
+- Invitation only "golden ticket" to world dance championship<br>
+</blockquote>
        </p>
       </div>
     </div>
     <div class="col-sm-4">
+      <img src="images/small/dancetothemusic_small.jpg" class="img-responsive" style="width:100%" alt="Image">
+      <img src="images/small/eternalflame16.JPG" class="img-responsive" style="width:100%" alt="Image">
+      <img src="images/small/heymickey16.jpg" class="img-responsive" style="width:100%" alt="Image">
+
        <img src="images/sliders_small/IMG_1977-001.JPG" class="img-responsive" style="width:100%" alt="Image">
        <img src="images/sliders_small/DSC_8056-001.JPG" class="img-responsive" style="width:100%" alt="Image">
        <img src="images/sliders_small/IMG_3415-001.JPG" class="img-responsive" style="width:100%" alt="Image">
@@ -53,14 +80,14 @@ Most Entertaining,  Secondary Large Group (Joy of Paper Cutting)
 4 National Top First Awards, Primary (Purple Fish, Joy of Learning Chinese, We've got the World, Jasmine Love)
 National Elite Top First Award, Secondary,  (Joy of Paper Cutting)
 Judges Choice Award ( (Joy of Paper Cutting)
-<em>Choreographer of the Year</em> (for Ms Jun Lu)        
+<em>Choreographer of the Year</em> (for Ms Jun Lu)
          </pre>
-         
+
           <b>American Dance Awards 2015</b><br>
          <pre>
 - "Ultimate Gold" to all participating routines (5)
-- First Place, High Score, 6 and under: Jasmine Love 
-- First Place, High Score, Primary, 12 and under: We've Got the World 
+- First Place, High Score, 6 and under: Jasmine Love
+- First Place, High Score, Primary, 12 and under: We've Got the World
 - First Place, High Score, Young, Large Group, Competitive: Joy of Paper Cutting
 - Second Place, High Score, Young, Large Group, Competitive: Joy of Learning Chinese
 - Fifth Place, High Score, Young, Large Group, Competitive: GongFu
@@ -68,32 +95,32 @@ Judges Choice Award ( (Joy of Paper Cutting)
 - Highest Adjudication, third place: Joy of Paper Cutting
 - "Mary Poppins" special award "Perfect in every way",  We've Got the World
 - "Garden of Talent" special award: Jasmine Love
-- "Animation" special award: Joy of Paper Cutting   
+- "Animation" special award: Joy of Paper Cutting
          </pre>
-         
+
 <b>KAR Regional 2015 at San Jose</b><br>
          <pre>
-First Place, Top Primary Large Group 8 and Under:  Jasmine Love 
-Second Place, Top Primary Large Group 8 and Under:  We've Got The World 
-Fourth Place, Top Primary Large Group 8 and Under:  Joy Of Learning Chinese 
+First Place, Top Primary Large Group 8 and Under:  Jasmine Love
+Second Place, Top Primary Large Group 8 and Under:  We've Got The World
+Fourth Place, Top Primary Large Group 8 and Under:  Joy Of Learning Chinese
 Fourth Place, Top Primary Large Group 9 - 11: Purple Fish
-First Place, Top Secondary Large Group 8 and Under: Joy Of Paper Cutting 
-Most Entertaining, Secondary Large Groups:  Joy Of Paper Cutting 
+First Place, Top Secondary Large Group 8 and Under: Joy Of Paper Cutting
+Most Entertaining, Secondary Large Groups:  Joy Of Paper Cutting
 4 Top First Awards
-Elite Top First for Joy Of Paper Cutting    
+Elite Top First for Joy Of Paper Cutting
          </pre>
        </p>
       </div>
-      
+
     </div>
-    
+
     <div class="col-sm-4">
       <img src="images/content/photo16.JPG" class="img-responsive" style="width:100%" alt="Image">
        <img src="images/content/photo7.JPG" class="img-responsive" style="width:100%" alt="Image">
        <img src="images/content/photo1.gif" class="img-responsive" style="width:100%" alt="Image">
     </div>
   </div>
-  
+
    <div id="news" class="row">
     <div class="col-sm-8">
       <div class="well">
@@ -106,7 +133,7 @@ National Elite Top First Award (Beat It)
 National Top First Award (Little Mulan)
 Most Entertaining, Secondary Large Group (Do the CanCan)
 High Point Award (Do the CanCan)
-High Point Award (Beat It)      
+High Point Award (Beat It)
 
 The award winning dancers are
 Do the CanCan: Shannon Ma, Ella Yee, Victoria Hu, Julie Shi, Jamie Tan, Ella Lan, Amy Liu, Sophia Wang, Ariel Huang, Katherine Chen. Maggie Peng
@@ -123,13 +150,13 @@ Do the CanCan
 -- High Score Awards, Age 7-9, First place
 -- Overall high score award, Primary, 12 and under, First place
 -- Special Judge Award : "Most Fun"
--- Overall Highest Adjudication, 12 & Under, first place 
--- "Diamond Choreography" 
+-- Overall Highest Adjudication, 12 & Under, first place
+-- "Diamond Choreography"
 
 Beat It
 -- Ultimate Gold Medal
 -- High score award, Age 7-9, Second place (note: "Do the CanCan" first place in this category)
--- Special Judge Award: "Most Intensity" 
+-- Special Judge Award: "Most Intensity"
 
 Little Mulan
 -- Ultimate Gold Medal
@@ -146,11 +173,11 @@ Ms Lu received the ADA's best choreography award again: "the ADA America's Chore
 And the team members are:
 (1) Do the CanCan (Shannon, Ella Y, Victoria, Julie, Amy, Ella L., Jamie, Sophia, Maggie, Katherine, Ariel)
 (2) Beat It (Ella Y, Victoria, Chloe, Ariel, Nina, Annabel, Adrienne, Amelia, Annie, Maggie, Angela, Katherine, Caroline, Macy, Vivian, Kelly)
-(3) Little Mulan (Mia Liu, Chloe Low, Lily Shi, Angela Pei, Daphne Zhu, Meredith Lin, Norina Xu, Claire Luo, Ella Li, Amy Miao, Sydney Li, Jessie Zhu) 
-(4) Galloping Spirit (Cecillia Sun, Aileen Wu, Elizabeth Ding, Nerine Uyanik, Emily Xia, Krystal Liu, Lily Jiang, Megan Xu, Caroline Wang, Kaylee Mai) 
+(3) Little Mulan (Mia Liu, Chloe Low, Lily Shi, Angela Pei, Daphne Zhu, Meredith Lin, Norina Xu, Claire Luo, Ella Li, Amy Miao, Sydney Li, Jessie Zhu)
+(4) Galloping Spirit (Cecillia Sun, Aileen Wu, Elizabeth Ding, Nerine Uyanik, Emily Xia, Krystal Liu, Lily Jiang, Megan Xu, Caroline Wang, Kaylee Mai)
 
          </pre>
-         
+
 <b>KAR Regional 2014 at San Jose</b><br>
          <pre>
 Do the CanCan
@@ -178,9 +205,9 @@ Hey Mickey
          </pre>
        </p>
       </div>
-      
+
     </div>
-    
+
     <div class="col-sm-4">
       <img src="images/small/CFC_9656c.JPG" class="img-responsive" style="width:100%" alt="Image">
        <img src="images/small/BeatIt.JPG" class="img-responsive" style="width:100%" alt="Image">
@@ -194,20 +221,20 @@ Hey Mickey
       <div class="well">
        <p>
          <h4> 2013 </h4>
-         
-<b>KAR Regional 2013 at San Jose</b><br>
-         <pre>         
-"Dance to the Music" won the "HIGH POINT AWARD (#1, highest)" across all styles and categories for SMALL groups ages 8 and under.  
-"Little Red Fans" won their own "HIGH POINT AWARD (#1, highest)" across all styles and categories for LARGE groups ages 8 and under.  
-"Happy Handkerchiefs" won "HIGH POINT AWARD(#5)" across all styles and categories for LARGE groups ages 8 and under.  
-"Dance of the Rainbow" won "HIGH POINT AWARD(#4)" in the solo, duo/trio category.  
-"Dance of the Rainbow" is also one of the four teams won "All Star Award" (only for dancers ages 9 and above) in the solo, duo/trio category.  
 
-"Dance to the Music" won the "MOST ENTERTAINING AWARD" across all styles and categories for SMALL groups (cross all ages and competitive levels).  
-"Little Red Fans" won the "MOST ENTERTAINING AWARD" across all styles and categories for LARGE groups (cross all ages and competitive levels).  
+<b>KAR Regional 2013 at San Jose</b><br>
+         <pre>
+"Dance to the Music" won the "HIGH POINT AWARD (#1, highest)" across all styles and categories for SMALL groups ages 8 and under.
+"Little Red Fans" won their own "HIGH POINT AWARD (#1, highest)" across all styles and categories for LARGE groups ages 8 and under.
+"Happy Handkerchiefs" won "HIGH POINT AWARD(#5)" across all styles and categories for LARGE groups ages 8 and under.
+"Dance of the Rainbow" won "HIGH POINT AWARD(#4)" in the solo, duo/trio category.
+"Dance of the Rainbow" is also one of the four teams won "All Star Award" (only for dancers ages 9 and above) in the solo, duo/trio category.
+
+"Dance to the Music" won the "MOST ENTERTAINING AWARD" across all styles and categories for SMALL groups (cross all ages and competitive levels).
+"Little Red Fans" won the "MOST ENTERTAINING AWARD" across all styles and categories for LARGE groups (cross all ages and competitive levels).
 that means, our teams sweep all "MOST ENTERTAINING AWARD"!  That is, there are only two "MOST ENTERTAINING AWARD" awards, and our dancers got them all!
 
-"Little Red Fans" won the regional  "Video of the Week" award. 
+"Little Red Fans" won the regional  "Video of the Week" award.
 "Happy Handkerchiefs" also won a judges' choice award as "the future's dancers"
 
 All of them won "Top first award" (highest) in their respective category
@@ -218,14 +245,14 @@ And the dancers are:
 (3) Happy Handkerchiefs (Ceci, Aileen, Caroline,Amy, Olivia)
 (4) Little Red Fans(Ella, Ariel, Amelia, Vivian, Adrienne, Victoria,  Lindawest, Annabel, Sophia, Annie, Maggie, Angela, Katherine, Caroline, Ella)
          </pre>
-         
+
           <b>American Dance Awards 2013</b><br>
          <pre>
 
-Three dance groups went to ADA 213 and won two "ULTIMATE GOLD", and one "GOLD". 
-All won the "HIGHEST OVERALL SCORE" awards in their respective competition category, 
+Three dance groups went to ADA 213 and won two "ULTIMATE GOLD", and one "GOLD".
+All won the "HIGHEST OVERALL SCORE" awards in their respective competition category,
 and one team won the "FIRST PLACE" award for all age 6 and under
-Two of the teams won "SPECIAL RECOGNITION" awards in addition for their outstanding performances. 
+Two of the teams won "SPECIAL RECOGNITION" awards in addition for their outstanding performances.
 Ms Lu received the best choreography award: "the ADA Diamond Choreography Winner of 2013".
 
 And the groups are:
@@ -233,9 +260,9 @@ And the groups are:
 (2) Little Red Fan (Ella, Ariel, Amelia, Vivian, Adrienne, Victoria,  Lindawest, Annabel, Sophia, Annie, Maggie, Angela, Katherine, Ella)
 (3) Happy Handkerchiefs (Ceci, Aileen, Caroline, Amy, Olivia, Angela)
 
-Congratulations to all the teams!  
+Congratulations to all the teams!
          </pre>
-         
+
        </p>
       </div>
     </div>
@@ -246,7 +273,7 @@ Congratulations to all the teams!
     </div>
   </div>
     <br>
- 
-  
-  
+
+
+
 </div><br>

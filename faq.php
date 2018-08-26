cfe3f84eb9c08f7fef53844aa2386e4fdbdfb550
Q Huang <q@hopelab.com>
2018-08-26 13:22:54 -0700

    Added FAQ

---
+++ faq.php
@@ -0,0 +1,109 @@
+<?php
+
+?>
+<div class="container">
+  <h3>------------- FAQs ---------------</h3><br>
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="well">
+<ul>
+
+<li><a href="#waitinglist">How do I signup for waiting list?</a></li>
+<li><a href="#whichclass">Which class/level is appropriate for my child?</a></li>
+<li><a href="#levelabcd">What does A,B mean in Level 1A and Level 1B?</a></li>
+<li><a href="#tryout">How do I signup for a try-out class?</a></li>
+<li><a href="#whattowear">What should my child wear when coming to the tryout/first class?</a></li>
+<li><a href="#buywear">Where should we buy the ballet outfit for class?</a></li>
+<li><a href="#team">Can my child join your competition team?</a></li>
+<li><a href="#styleofdance">What style of dance do you teach in each class?</a></li>
+<li><a href="#performance">Can my child join school performance?</a></li>
+<li><a href="#moreq">What if I have more questions?</a></li>
+</ul>
+
+
+       </div>
+    </div>
+  </div>
+
+
+
+
+<div class="row">
+
+<ul class="faq">
+<li id="waitinglist">How do I signup for waiting list?</li>
+The same as signing up for a regular class. Once the respective
+class is full, you will automatically be put into a waiting list.
+<br>  <br>
+
+<li id="whichclass">Which class/level is appropriate for my child?</li>
+We use age and experience as criteria for placing students.
+Normally for a 3~5 years old beginning child, our level 1 is the appropriate level
+for her/him to get started.
+For a 6 years old, level 2 would be appropriate, and so on. If you child has quite a bit a prior dance experience/training,
+then we need to have a teacher to evaluate her/him to see which class is a best fit.
+<br>  <br>
+
+<li id="levelabcd">What does A,B mean in Level 1A and Level 1B?</li>
+The letters A,B,C,D in our dance class description after the level
+number is just for the convenience of noting
+different classes at the same level.
+<br>  <br>
+
+<li id="tryout">How do I signup for a try-out class?</li>
+The same as signing up for a regular class. In the signup notes,
+please note your try out request.
+The teacher of the respective class will send you email regarding
+which day to come to do the try out. Our teachers will limit the
+tryout numbers in each class on first come first serve
+basis, so tryout might not be immediately available.
+<br>  <br>
+
+<li id="whattowear">What should my child wear when coming to the tryout/first class?</li>
+For levels 3 and below classes, pink ballet outfit with pink ballet shoes will be appropriate.
+For levels 4 and up, black leotard with pink ballet shoes will be appropriate.
+<br>  <br>
+
+<li id="buywear">Where should we buy the ballet outfit for class?</li>
+  Almost any dance wear store.
+  Our studio have limited stock of ballet outfit with ballet shoes as well.
+  (For levels 3 and under, you shall buy the ballet tutu from us, unless you
+  find a style that matches our standard tutu style for levels 3 and under.)
+  <br>
+    <br>
+
+<li id="team">Can my child join your competition team?</li>
+All our dance competition team members needs to pass our audition.
+Once your child passes the audition, she/he might need to take special private
+lessons to catch up with the team routine progress (if you child did not join
+when the team is formed in the summer).
+<br>  <br>
+
+<li id"styleofdance">What style of dance do you teach in each class?</li>
+All of our dance classes (except ballet) are combo classes.
+It include basic technique training that includes basic ballet, then a performance part.
+The performance part is depending on the levels and each class might be different.
+Our styles range from eastern and western folk dances to hiphop/k-pop, jazz,
+flamenco, lyrical, broadway, and modern dances.  For younger child, the
+dance style is not very distinguished, more of a "happy and lovely" dance style.
+<br>  <br>
+<li id="performance">Can my child join school performance?</li>
+  Yes. We encourage and arrange all dancers in our classes
+  to participate our annual recital performance.
+  However, for a new classes that is formed
+  at a time that is too close to the performance date, when it is unlikely
+  have enough time for the routine and costumes ready for the performance,
+  we will let the class know and the class will work towards a
+  later performance.
+
+<br>  <br>
+<li id="moreq">What if I have more questions?</li>
+You may email info@jludance.com or call us.
+<br>
+</ul>
+
+
+
+</div>
+
+</div>

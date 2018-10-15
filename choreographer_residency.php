7b4122cfa90f283c1232dc12efd5561bbb654eb7
Q Huang <q@hopelab.com>
2018-10-15 09:38:44 -0700

    added choreographer residency initial wording

--- choreographer_residency.php
+++ choreographer_residency.php
@@ -2,22 +2,31 @@
 
 ?>
 <div class="container text-center">
-  <h3>Job Openings</h3><br>
+  <h3>The Jun Lu Choreographer Residency</h3><br>
   <div class="row">
     <div class="col-sm-12">
       <div class="well">
 
-         <h4> 我们随时欢迎优秀的舞蹈老师加入我们的教师团队 </h4>
+         <h4> 我们也欢迎喜欢编舞的舞者来申请短期住院编舞基金 </h4>
 
-         We welcome passionate dance instructors (from teaching assistant to master level teachers)
-         to join our growing team team.  If you love dance, loves to teach dance,
-         can make classes fun and inspire progress, we love to hear from you.
+Jun Lu Performing Arts Academy is pleased to announce an ongoing opportunity for 
+new and emerging choreographers. All interested choreographers are invited to apply 
+to work with dancers and teachers from Jun Lu Performing Arts at the school’s fully 
+equipped San Jose studios, for a minimum of 5 hours, Monday-Thursday morning or early afternoons.  
+One to three week residencies are available now through June 2019, dates TBD. 
+A video tape of the work will be recorded on the last session for archival and promotional purposes, 
+and footage will be provided to the choreographer. 
+A $500 honorarium will be paid to each selected choreographer. 
+Rehearsals will take place at the Jun Lu Performing Arts Academy studios. 
+<br><br>
+Interested applicants should email a work sample video of approximately 5 minutes of choreography, 
+as well as a project description of the work they would like to focus on throughout the time of 
+the residency. Project descriptions should include number of dancers needed and an indication of the style, intent, 
+or focus of the choreography.  No more than 500 words please.
+<br><br>
+Please submit video link and project description, along with your name, email address, 
+a short bio or resume and your phone number to info@jludance.org 
 <br>
-         If you are trained in ballet, musical theater, Jazz, Lyrical, various forms of Hiphop, tap,
-         or folk dances from around the world, and think you can work with our dedicated
-         professional team to make our dancers smile more and shine brighter, please feel free
-         to contact us at any time.
-         <h3>Email: teach@jludance.com</h3>
 
        </div>
     </div>

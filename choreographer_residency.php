3ce3bd9550afa6707cf50b5d54bbd420e10c88a6
Q Huang <q@hopelab.com>
2018-10-14 17:16:03 -0700

    added choreographer residency initial wording

---
+++ choreographer_residency.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+<div class="container text-center">
+  <h3>Job Openings</h3><br>
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="well">
+
+         <h4> 我们随时欢迎优秀的舞蹈老师加入我们的教师团队 </h4>
+
+         We welcome passionate dance instructors (from teaching assistant to master level teachers)
+         to join our growing team team.  If you love dance, loves to teach dance,
+         can make classes fun and inspire progress, we love to hear from you.
+<br>
+         If you are trained in ballet, musical theater, Jazz, Lyrical, various forms of Hiphop, tap,
+         or folk dances from around the world, and think you can work with our dedicated
+         professional team to make our dancers smile more and shine brighter, please feel free
+         to contact us at any time.
+         <h3>Email: teach@jludance.com</h3>
+
+       </div>
+    </div>
+    <br>
+
+</div>
+
+</div><br>

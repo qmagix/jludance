aa78654e6cf7f01e27d79694e7b658e7372737e3
Q Huang <q@hopelab.com>
2018-10-28 13:26:21 -0700

    added team 3 link created by Grace

--- gallery.php
+++ gallery.php
@@ -14,15 +14,21 @@
 	    </div>
     </div>  
     <div class="row">
+    	<div class="col-sm-6">
+	      <h4 align="center">From Cowboys to Jasmine by Team 3</h4><center>
+	      <iframe width="560" height="315" src="https://www.youtube.com/embed/5ix_D0GVAAk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>	    
+	    </div>
 	    <div class="col-sm-6">
 	    <h4 align="center">The Year of Panda by Team 4</h4><center>
 	      <iframe width="560" height="315" src="https://www.youtube.com/embed/5dUEFrl6rFY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 	     </div>
-	    <div class="col-sm-6">
+    </div>  
+        <div class="row">
+        <div class="col-sm-6">
 	      <h4 align="center">From Princess to Soldiers by Team 5</h4><center>
 	      <iframe width="560" height="315" src="https://www.youtube.com/embed/1rM31bl4RnU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 	    </div>
-    </div>  
+	     </div>  
         <div class="row">
 	    <div class="col-sm-6">
 	     <h4 align="center">2017 Recital Trailer</h4>
@@ -35,7 +41,7 @@
     </div>  
     <div class="row">
 	    <div class="col-sm-6">
-	     <h4 align="center">Moements in Theater by Shannon Ma</h4>
+	     <h4 align="center">Moments in Theater by Shannon Ma</h4>
 	     <iframe width="560" height="315" src="https://www.youtube.com/embed/G_ED-EewHB4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 	     </div>
 	    <div class="col-sm-6">

c53f9ba9c238e980c89675bc86556b1012937098
Q Huang <q@hopelab.com>
2019-08-20 20:40:08 -0700

    db test

--- schedule.php
+++ schedule.php
@@ -12,5 +12,37 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+$today=date('Y-m-d');
+$sql="SELECT * FROM l5_study_groups WHERE ending_date>".$today." ORDER BY title ASC";
+$link=$conn;
+
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>id</th>";
+                echo "<th>title</th>";
+                echo "<th>ending_date</th>";
+            echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['title'] . "</td>";
+                echo "<td>" . $row['ending_date'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Close result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+// Close connection
+mysqli_close($link);
+
 
 ?>

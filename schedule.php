f96580d0980b584310fff1e4f6b5f224eb21f37f
Q Huang <q@hopelab.com>
2019-08-20 20:29:48 -0700

    updated slider text and testing db conn

--- schedule.php
+++ schedule.php
@@ -1,85 +1,16 @@
 <?php
+$DB_HOST="localhost";
+$DB_PORT='3306';
+$DB_DATABASE='l5studio';
+$DB_USERNAME='l5studio';
+$DB_PASSWORD='qingfeng';
 
-include("header.php");
-$page_id="schedule";
-include("navbar.php");
-include("carousel.php");
+$conn = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
 ?>
-
-<div class="container text-center">
-  <h3>Jun Lu Performing Arts Academy</h3><br>
-
-  <div class="row">
-    <div class="col-sm-12">
-      <div class="well">
-       <p>
-        <table width="100%"><tr><td valign=top><h2>Kids' Classes</h2><h3>Saturday</h3>
-
-Dance Level 6B,  9:00 - 11:00am<br/>
-Dance Level 2A,  9:30 - 11:00am<br/>
-Dance Level 8A,  11:00 -1:00pm<br/>
-Dance Level 3A,  11:15-12:45pm<br/>
-Dance Level 1A,  1:00 - 2:00pm<br/>
-Dance Level 5B,  1:00p - 3:00pm<br/>
-Dance Level 4A,  2:30p - 4:30pm<br/>
-Dance Level 10,  3:00p - 5:00pm<br/>
-Dance Level 9B,  5:00p - 7:00pm<br/>
-
-
-<h3>Sunday</h3>
-Dance Level 2B,  9:30 - 11:00am<br/>
-Dance Level 7B,  11:00 - 1:00pm<br/>
-Dance Level 4B,  11:15 - 1:15pm<br/>
-Dance Level 8B,  1:00p - 3:00pm<br/>
-Dance Level 3B,  1:30p - 3:00pm<br/>
-Dance Level 1B,  3:00p - 4:00pm<br/>
-Dance Level 5A,  3:00p -5:00pm<br/>
-
-
-<h3>Monday</h3>
-Dance Level 2C, 5:45p - 7:15pm<br/>
-Dance Level 4C, 6:45p - 8:45pm<br/>
-
-<h3>Tuesday</h3>
-Competition Team Training 1,  5:45-7:45pm<br/>
-Dance Level 3C, 6:00p - 7:30pm<br/>
-<h3>Wednesday</h3>
-Competition Team Training 3,  4:00-6:00pm<br/>
-Dance Level 7A,  6:00p - 8:00pm<br/>
-<h3>Thursday</h3>
-Dance Level 1C, 6:15p - 7:15pm<br/>
-Competition Team Training 2,  5:45-7:45pm<br/>
-<h3>Friday</h3>
-Dance Level 9A,  5:45 - 7:45pm<br/>
-Team Ballet, 5:45 - 7:15pm<br/>
-Dance Level 6A,  7:15 - 9:15pm<br/>
-
-
-</td><td valign=top><h2>Adults' Classes</h2><h3>Sunday</h3>
-Adult Level 2,  9 - 11:00am<br/>
-<h3>Tuesday</h3>Adult beginner,  10:00 - 11:30am<br/>
-<h3>Tuesday</h3>Adult Level 2,  7:30 - 9:30pm<br/>
-<h3>Wednesday</h3>Adult Level 2,  7:45 - 9:45pm<br/>
-<h3>Thursday</h3>Adult Level 3,  7:30 - 9:30pm<br/>
-<h3>Friday</h3>Adult Beginner,  9:30-11:00am<br/>
-</td>
-</tr></table>
-       </p>
-      </div>
-    </div>
-
-  </div>
-  <div class="row">
-    <div class="col-sm-6">
-      <img src="images/content/photo1.gif" class="img-responsive" style="width:100%" alt="Image">
-      <p>Children</p>
-    </div>
-    <div class="col-sm-6">
-      <img src="images/content/photo4.jpg" class="img-responsive" style="width:100%" alt="Image">
-      <p>Adults</p>
-    </div>
-  </div>
-</div><br>
-
-<?php include("footer.php");?>

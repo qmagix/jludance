e50aaf42ada5169ae084535dc04dab17965febd4
Q Huang <q@hopelab.com>
2019-09-19 15:12:20 -0700

    updated job posting

--- jobs.php
+++ jobs.php
@@ -18,7 +18,7 @@
          professional team to make our dancers smile more and shine brighter, please feel free
          to contact us at any time.
          <h3>Email: teach@jludance.com</h3>
-
+ --------
          <h4> 我们随时欢迎优秀的舞蹈老师加入我们的教师团队 </h4>
 
        </div>

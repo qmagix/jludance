ee5f649c16de7702212c78d9b38d0e232c90aebb
Qingfeng <qingfeng@Qingfengs-Pro.attlocal.net>
2020-01-08 19:29:46 -0800

    updated newsletter

--- newsletter.php
+++ newsletter.php
@@ -11,6 +11,8 @@
 
          We are happy to start a school newsletter to share various news among our student and alumnus families.
          Here is a link to our first newsletter:
+         <h4><a href="https://mailchi.mp/eb33c5093e9c/jludances-dancer-and-alumni-family-newsletter-2020-1">Dancer and Alumni Family Newsletter 2020-1, Jan 2020</a></h4>
+
          <h4><a href="https://mailchi.mp/f1eaaec3244f/jludances-dancer-and-alumni-family-newsletter-4">Dancer and Alumni Family Newsletter #4, Nov 2019</a></h4>
           <h4><a href="https://mailchi.mp/76ff719e3293/jludances-dancer-and-alumni-family-newsletter-3">Dancer and Alumni Family Newsletter #3, Oct 2019</a></h4>
          <h4><a href="https://mailchi.mp/5573c7708855/jludances-dancers-and-alumni-family-newsletter-2">Dancer and Alumni Family Newsletter #2, Oct 2019</a></h4>

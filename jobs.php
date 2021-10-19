942c0f92b10445f0605688d41906ef59b70d1897
Qingfeng <qingfeng@Qingfengs-Mini.attlocal.net>
2021-10-18 20:46:56 -0700

    updated jos

--- jobs.php
+++ jobs.php
@@ -10,8 +10,11 @@
          <h2>Ballet, Jazz, Musical Theater, Hiphop, Folk, Modern, Gymnastic.... </h2>
 
          We welcome passionate dance instructors (from teaching assistant to master level teachers)
-         to join our growing team.  If you love dance and music, loves to teach,
-         can make classes fun and inspire progress, we love to hear from you.
+         to join us to build one of the best dance studios together in the Sunny California, to innovate 
+         and create a great learning enviroment for the stduents, and to bring the world closer together 
+         through the language of dance art.  <br>
+         
+         If you love dance and music, loves to teach, can make classes fun and inspire progress, we love to hear from you.
 <br>
          If you are trained in ballet, musical theater, Jazz, Lyrical, various forms of Hiphop, tap,
          or folk dances from around the world, and think you can work with our dedicated
@@ -19,7 +22,7 @@
          to contact us at any time.
          <h3>Email: teach@jludance.com</h3>
  --------
-         <h4> 我们随时欢迎优秀的舞蹈和音乐老师加入我们的教师团队 </h4>
+         <h4> 我们随时欢迎优秀的舞蹈和音乐老师加入和我们一起打造一家全美最优质的舞蹈艺术学校！一起用东西方的艺术和创新架好一条独特的心灵文化桥梁。 </h4>
 
        </div>
     </div>
